5625675fb1befa35d8df3dff8549d3304b9bbb4c
camperitox <ml17017@ues.edu.sv>
2022-09-08 19:52:39 -0600

    🔧 Se genereron los filtros

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MenuComponent } from './components/menu/menu.component';
 import { DetalleComponent } from './components/detalle/detalle.component';
 import { BaseComponent } from './layouts/base/base.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import { HttpClientModule } from '@angular/common/http';
 import {MatCardModule} from '@angular/material/card';
+import {MatIconModule} from '@angular/material/icon';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatInputModule} from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
 import {MatButtonModule} from '@angular/material/button';
-import {MatFormFieldModule} from '@angular/material/form-field';
-
 @NgModule({
   declarations: [
     AppComponent,
@@ -26,8 +31,12 @@ import {MatFormFieldModule} from '@angular/material/form-field';
     MatToolbarModule,
     HttpClientModule,
     MatCardModule,
-    MatButtonModule,
-    MatFormFieldModule
+    MatIconModule,
+    MatGridListModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    MatSelectModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { PaisesApiService } from 'src/app/services/paises-api.service';
 import { Router } from '@angular/router';
 import { map, startWith } from 'rxjs';
+//Filtro
+import { FormBuilder } from '@angular/forms';
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
@@ -10,9 +12,17 @@ import { map, startWith } from 'rxjs';
 export class MenuComponent implements OnInit {
   //definiendo variables
   paisesList:any[]=[];
+  //definiendo variables para el filtrado
+  paisesFiltrados: any;
+  buscadorPais: any = this.fb.control('');
+  selectPais: any = this.fb.control(null);
+  regiones:any;
+
   constructor(
     private paisesApiService: PaisesApiService,
     private router: Router,
+    //se usa para el filtro
+    private fb: FormBuilder
   ) { }
 
   ngOnInit(): void {
@@ -33,9 +43,55 @@ export class MenuComponent implements OnInit {
           };
         });
         console.log(this.paisesList)
+        this.paisesFiltrados = this.paisesList;
+        this.regiones = [
+          ...new Set(resultado.map((result: any) => result.region)),
+        ];
+
       },
       error: (error) => {},
       complete: () => {},
     });
+  }
+
+  filtro(filtro: string) {
+    const valor = filtro.toLowerCase();
+    this.paisesFiltrados =
+      this.selectPais.value === null
+        ? this.paisesList.filter((e: any) =>
+            e.nombrePais.toLowerCase().includes(valor)
+          )
+        : this.paisesList.filter(
+            (e: any) =>
+              e.nombrePais.toLowerCase().includes(valor) &&
+              e.region.toLowerCase() === this.selectPais.value.toLowerCase()
+          );
+  }
+
+  filtroPorRegion(filtro: string) {
+    const valor = filtro ? filtro.toLowerCase() : '';
+    //console.log(!filtro);
+    if (filtro) {
+      this.paisesFiltrados =
+        this.buscadorPais.value !== ''
+          ? this.paisesList.filter(
+              (e: any) =>
+                e.region.toLowerCase() === valor &&
+                e.nombrePais
+                  .toLowerCase()
+                  .includes(this.buscadorPais.value.toLowerCase())
+            )
+          : this.paisesList.filter(
+              (e: any) => e.region.toLowerCase() === valor
+            );
+    } else if (this.buscadorPais.value !== '') {
+      this.paisesFiltrados = this.paisesList.filter((e: any) =>
+        e.nombrePaiss
+          .toLowerCase()
+          .includes(this.buscadorPais.value.toLowerCase())
+      );
+    } else {
+      this.paisesFiltrados = this.paisesList;
+    }
   }
 }
